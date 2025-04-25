4e587753c15cac161943586af744c57f6fe68d5d
SMtz <mtz.stian@gmail.com>
2025-04-25 15:33:13 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     enablePopovers: true,
     analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "techofcriticalpedagogy.sebastianms.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
